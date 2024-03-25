67dab044a92b8020762324cbf9095161aaeb0445
Asus Zenbook <anapiiaevsandary@gmail.com>
2024-03-25 18:11:40 +0300

    let's

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("write your age");
+        int age=scanner.nextInt();
     }
 }
